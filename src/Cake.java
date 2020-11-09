89586b7c39468378cb4ca82fab711ad3ffd1b1b8
Luke Powers <lpowers@ou.edu>
2020-11-09 16:32:29 -0600

    We can eat cake now!

--- src/Cake.java
+++ src/Cake.java
@@ -1,4 +1,21 @@
 
 public class Cake {
 
+	private int bites = 10;
+	public Cake() {
+		
+		
+	}
+	
+	public void takeBite() {
+		
+		bites--;
+		System.out.println("Mmmmmm..... (this is a lie)");
+		System.out.println(bites);
+	}
+	
+	public boolean isEmpty() {
+		
+		return bites == 0;
+	}
 }
---
+++ src/EatCake.java
@@ -0,0 +1,26 @@
+
+public class EatCake {
+	
+	public EatCake() {
+		
+	}
+	
+	public static void eatCake(Cake cake) {
+		
+		if(!cake.isEmpty()) {
+			cake.takeBite();
+			eatCake(cake);
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		Cake cake = new Cake();
+		
+		eatCake(cake);
+		
+		System.out.println("Done. Noice");
+
+	}
+
+}
